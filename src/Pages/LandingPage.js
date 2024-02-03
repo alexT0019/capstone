681dbba85c36038036b354115f87b103ea2fc2a3
AndreaC <acchengandrea@gmail.com>
2024-02-03 16:50:10 +0800

    andrea

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,11 +1,13 @@
 import './landingPage.css';
 import Button from '@mui/material/Button';
+import pic1 from './schoolBagGlobeRocket.png';
+
 function LandingPage() {
     return(
       <div class="flex-container">
         <div class="flex-item-left">
-          <img 
-            src="/src/Pages/schoolBagGlobeRocket.png"
+          <img style={{ width: 822, height: 514, alignSelf: 'center'}}
+            src={pic1}
             alt="SchoolbagRocketGlobe"
             />
         </div>
@@ -16,8 +18,7 @@ function LandingPage() {
             <p>Learn Cantonese in a fun and interactive way with our innovative AR Integration feature.</p>
             <p>Sign up NOW to track your learning progress, get feedback and achievements!</p>
             <Button variant="contained">SIGN UP HERE</Button>
-          </div>
-          
+          </div>          
       </div>
     );
 }
