5d4dc8caa696344177889e21a85074c43c2c0f98
Alex Tsui <ying3438@gmail.com>
2024-02-04 23:33:09 +0800

    Completed nav barv bar

--- src/App.js
+++ src/App.js
@@ -5,27 +5,42 @@ import UserRegistration from "./Pages/UserRegistration";
 import UserProfile from "./Pages/UserProfile";
 import AugmentedReality from "./Pages/AugmentedReality";
 import ProgressTracking from "./Pages/ProgressTracking";
-
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
    <div>
     {/* navbar */}
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="signup">Get Started</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/arlearning">AR Learning</Link>
-      <Link to="/progress">Progress Tracking</Link>
+    <nav className="navStyle">
+      <Link className="linkStyle" to="/">Home</Link>
+      <Link className="linkStyle" to="/profile">Profile</Link>
+      <Link className="linkStyle" to="/arlearning">AR Learning</Link>
+      <Link className="linkStyle" to="/progress">Progress Tracking</Link>
+      <Button className="linkButtonStyle">
+        <Link className="linkStyle" to="/signup">Get Started</Link>
+      </Button>
     </nav>
     {/* routes */}
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/signup" element={<UserRegistration />}></Route>
         <Route path="/profile" element={<UserProfile />}></Route>
         <Route path="/arlearning" element={<AugmentedReality />}></Route>
         <Route path="/progress" element={<ProgressTracking />}></Route>
+        <Route path="/signup" element={<UserRegistration />}></Route>
       </Routes>
+    {/* footer */}
+    <footer className="footerStyle">
+      <Link className="footerLinkStyle" to="/">Home</Link>
+      <span className="footerContainer">
+        <Link className="footerLinkStyle" to="/profile">Profile</Link>
+        <span className="footerContainer2">
+          <Link className="footerLinkStyle2" to="/signup">Get Started</Link>
+          <Link className="footerLinkStyle2" to="/progress">Progress Tracking</Link>
+        </span>
+      </span>
+      <Link className="footerLinkStyle" to="/arlearning">AR Learning</Link>
+      <span className="footerCopyright">Copyright 2024©All rights reserved</span>
+    </footer>
    </div>
 
   );
--- src/Pages/UserRegistration.js
+++ src/Pages/UserRegistration.js
@@ -1,4 +1,48 @@
+import "./UserRegistration.css"
+import picture from "../Images/userRegistration.png"
+
 function UserRegistration() {
-    return;
+    return (
+      <div>
+        <div className="user-registration">
+          <div className="rectangle-27"></div>
+          <div className="group-1">
+            <div className="rectangle-12"></div>
+            <div className="email">Email </div>
+            <div className="rectangle-6"></div>
+            <div className="password">Password </div>
+            <div className="rectangle-8"></div>
+            <div className="username">Username </div>
+            <div className="rectangle-7"></div>
+            <div className="sign-up">Sign Up </div>
+            <div className="already-have-an-account-sign-in-now">
+              <span>
+                <span className="already-have-an-account-sign-in-now-span">
+                  Already have an account?{" "}
+                </span>
+                <span className="already-have-an-account-sign-in-now-span2">
+                  Sign in now
+                </span>
+              </span>{" "}
+            </div>
+            <div className="submit">SUBMIT </div>
+          </div>
+          <div className="get-started">Get Started </div>
+          <div className="progress-tracking">Progress Tracking </div>
+          <div className="home">Home </div>
+          <div className="profile">Profile </div>
+          <div className="ar-learning">AR Learning </div>
+          <div className="copyright-2024-all-rights-reserved">
+            Copyright 2024©All rights reserved{" "}
+          </div>
+          <div className="rectangle-13"></div>
+          <div className="get-started2">Get Started </div>
+          <div className="home2">Home </div>
+          <div className="profile2">Profile </div>
+          <div className="ar-learning2">AR Learning </div>
+          <img className="ar-project-1" src={picture} alt="schoolbag" />
+        </div>
+      </div>
+    );
 }
 export default UserRegistration;
