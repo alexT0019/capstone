0e9b9582834788cff028ce7c32decaeeea98ef04
julia <julialaw0701@gmail.com>
2024-02-10 15:24:38 +0800

    add button.css for the progress tracking

--- src/Pages/ProgressTracking.js
+++ src/Pages/ProgressTracking.js
@@ -1,4 +1,56 @@
+import { useEffect, useState } from 'react';
+
 function ProgressTracking() {
-    return;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <div>
+      <h1>Progress Tracking</h1>
+      <button className="button">Learning Record</button>
+      <p></p>
+      <span className="animals">Animals</span>
+      <p></p>
+      <table>
+        <tr>
+          <td>Tiger</td>
+          <td>老虎</td>
+        </tr>
+        <tr>
+          <td>Dog</td>
+          <td>狗</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Cat</td>
+          <td>貓</td>
+          <td></td>
+        </tr>
+      </table>
+      <p></p>
+      <span className="fruit">Fruit</span>
+      <p></p>
+      <table>
+        <tr>
+          <td>Orange</td>
+          <td>橙</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Apple</td>
+          <td>蘋果</td>
+        </tr>
+        <tr>
+          <td>Kiwi</td>
+          <td>奇異果</td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+  );
 }
+
 export default ProgressTracking;
---
+++ src/Pages/ScoreReward.js
@@ -0,0 +1,10 @@
+function ScoreReward() {
+    return (
+      <div>
+        <h1>Score Reward</h1>
+        <img src="./capstone/src/Pages/ScoreReward.png" alt="Score Reward" />
+      </div>
+    );
+  }
+  
+  export default ScoreReward;
