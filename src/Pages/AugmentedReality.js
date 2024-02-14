4763ef9873a792b45edc54f0d774243d12e982d9
janielala <janielaudesign@gmail.com>
2024-02-14 19:58:46 +0800

    Co-authored-by: dugalcedo <dugalcedo@users.noreply.github.com>

--- src/Pages/AugmentedReality.js
+++ src/Pages/AugmentedReality.js
@@ -1,4 +1,11 @@
+
 function AugmentedReality() {
-    return;
+    return(
+        <div className="App">
+        <h3>Iframes in React</h3>
+        <iframe src="https://xr.plus/rfs" width={1000} height={500}></iframe>
+      </div>
+    );
+
 }
 export default AugmentedReality;
